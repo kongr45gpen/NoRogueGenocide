3faa36eceb0b63317911b07bb9fd201179d89213
kongr45gpen <kongr45gpen@helit.org>
2013-06-12 22:31:26 +0300

    Make the plugin functional

--- NoRogueGenocide.cpp
+++ NoRogueGenocide.cpp
@@ -1,16 +1,17 @@
 // NoRogueGenocide.cpp : Defines the entry point for the DLL application.
 //
 
+#include <iostream>
 #include "bzfsAPI.h"
 #include "plugin_utils.h"
 
 class NoRogueGenocide : public bz_Plugin
 {
 public:
-  virtual const char* Name (){return "SAMPLE PLUGIN";}
+  virtual const char* Name (){return "No Rogue Genocide";}
   virtual void Init ( const char* config);
 
-  virtual void Event ( bz_EventData * /* eventData */ ){return;}
+  virtual void Event ( bz_EventData *  eventData );
 };
 
 BZ_PLUGIN(NoRogueGenocide)
@@ -18,9 +19,52 @@ BZ_PLUGIN(NoRogueGenocide)
 void NoRogueGenocide::Init ( const char* /*commandLine*/ )
 {
   bz_debugMessage(4,"NoRogueGenocide plugin loaded");
-
-  // init events here with Register();
+  
+  Register(bz_eFlagGrabbedEvent);
+  Register(bz_eShotFiredEvent);
 }
+
+void NoRogueGenocide::Event ( bz_EventData * eventData) 
+{
+  const char* geno = "G";
+
+  switch (eventData->eventType) {
+    case bz_eFlagGrabbedEvent: {
+      bz_FlagGrabbedEventData_V1	*grabData = (bz_FlagGrabbedEventData_V1*)eventData;
+
+     
+      // Do nothing if the flag isn't Genocide
+      if (bz_getFlagName(grabData->flagID).c_str() != geno) return;
+     
+      // Do nothing if the player is not a rogue
+      if (bz_getPlayerTeam(grabData->playerID) != eRogueTeam) return;
+
+      //bz_removePlayerFlag(grabData->playerID);
+
+    } break;
+    
+    case bz_eShotFiredEvent: {
+      bz_ShotFiredEventData_V1	*shotData = (bz_ShotFiredEventData_V1*)eventData;
+
+      const char* shotType = shotData->type.c_str();
+
+      // Do nothing if the shot doesn't originate from a geno flag
+      if (strcmp(shotType,geno) != 0) return;
+
+      // Do nothing if the player is not a rogue
+      if (bz_getPlayerTeam(shotData->playerID) != eRogueTeam) return;
+
+      // Convert the shot to a regular one
+      shotData->type = "";
+      shotData->changed = true;
+
+    } break;
+
+    default:
+    return;
+  }
+}
+
 // Local Variables: ***
 // mode:C++ ***
 // tab-width: 8 ***
