52a81f7232862abc9f8883c81b3573ace1bc4443
kongr45gpen <kongr45gpen@helit.org>
2013-06-21 15:25:08 +0300

    Use std::strings instead of const char*s

--- NoRogueGenocide.cpp
+++ NoRogueGenocide.cpp
@@ -12,7 +12,7 @@ public:
   virtual void Init ( const char* config);
 
   virtual void Event ( bz_EventData *  eventData );
-  
+
   bool silentMode;
   bool seamlessMode;
 };
@@ -22,32 +22,31 @@ BZ_PLUGIN(NoRogueGenocide)
 void NoRogueGenocide::Init ( const char* commandLine )
 {
   bz_debugMessage(4,"NoRogueGenocide plugin loaded");
-  
+
   std::string param = bz_tolower(commandLine);
 
   silentMode = (param == "silent" || param == "quiet");
   seamlessMode = (param == "seamless" || param == "hidden");
-  
+
   Register(bz_eFlagGrabbedEvent);
   Register(bz_eShotFiredEvent);
 }
 
-void NoRogueGenocide::Event ( bz_EventData * eventData ) 
+void NoRogueGenocide::Event ( bz_EventData * eventData )
 {
-  const char* geno = "G";
+  std::string geno = "G";
 
   switch (eventData->eventType) {
     case bz_eFlagGrabbedEvent: {
       bz_FlagGrabbedEventData_V1	*grabData = (bz_FlagGrabbedEventData_V1*)eventData;
 
-      const char* flagType = bz_getFlagName(grabData->flagID).c_str();
+      std::string flagType = bz_getFlagName(grabData->flagID).c_str();
 
       // Do nothing if we are on seamless mode
       if (seamlessMode) return;
-      std::cout<<seamlessMode<<std::endl;
 
       // Do nothing if the flag isn't Genocide
-      if (strcmp(flagType,geno) != 0) return;
+      if (flagType != geno) return;
 
       // Do nothing if the player is not a rogue
       if (bz_getPlayerTeam(grabData->playerID) != eRogueTeam) return;
@@ -57,20 +56,20 @@ void NoRogueGenocide::Event ( bz_EventData * eventData )
       bz_removePlayerFlag(grabData->playerID);
 
     } break;
-    
+
     case bz_eShotFiredEvent: {
       bz_ShotFiredEventData_V1	*shotData = (bz_ShotFiredEventData_V1*)eventData;
 
-      const char* shotType = shotData->type.c_str();
+      std::string shotType = shotData->type.c_str();
 
       // Do nothing if the shot doesn't originate from a geno flag
-      if (strcmp(shotType,geno) != 0) return;
+      if (shotType != geno) return;
 
       // Do nothing if the player is not a rogue
       if (bz_getPlayerTeam(shotData->playerID) != eRogueTeam) return;
 
       // Convert the shot to a regular one
-      shotData->type = "";
+      shotData->type = "MG";
       shotData->changed = true;
 
     } break;
--- README.txt
+++ README.txt
@@ -4,4 +4,10 @@
 
 This is the NoRogueGenocide plugin.
 
-It prevents rogues from getting or using the Genocide (G) flag.
+Replace every occurrence of the term NoRogueGenocide in every file in here
+with the name of your project.  Also open .sln and .vcproj files in a
+text editor and do a search and replace.  Then write a short readme with
+usage instructions or other details in this file. If you did not use the
+newplug.sh script to create this directory, add the appropriate entries
+to configure.ac and plugins/Makefile.am. The plugin will have to be
+manually added to the mac Xcode project.
